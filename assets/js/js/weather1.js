e01514a0ca8328efeaf124ee86186ff60eae33cb
AdibaSjd <adibasjd.id@gmail.com>
2022-11-29 21:38:43 +0000

    js

--- assets/js/js/weather1.js
+++ assets/js/js/weather1.js
@@ -11,14 +11,13 @@ function getlocation() {
 
     // if empty throw error
     if (userlocation === '') {
-        setlocationError("Enter a location");
+        setLocationError("Enter a location");
         // setLocationError("Enter a location");
     } else {
-        findlocation(userlocation);
+        findlocation (userlocation);
     }
 }
-
-function setLocationError(message){
+function setLocationError(message) {
     document.queryselector("#error").textContent = message;
 }
 
@@ -45,7 +44,7 @@ const searchlocation = function (search) {
 //current weather displayed 
 const displayCurrentWeather = function (data) {
     const currentWeather = data.current;
-
+console.log(currentWeather);
     document.querySelector('#temp_value').textContent = `${currentWeather.temp}`;
     document.querySelector('#wind_value').textContent = `${currentWeather.wind_speed}MPH`;
     document.quwryselector('#humid_value').textContent = `${currentWeather.humididity}%`;
@@ -56,7 +55,7 @@ const displayCurrentWeather = function (data) {
 
 function getWeather (lat, lon) {
 
-    const apiUrl = `https://api.openweathermap.org/data/2.5/onecall?lat=${lat}&lon=${lon}&units=imperial&appid=dce906a6d5620fa7b5e261e8bf5ff913`;
+    const apiUrl = `https://api.openweathermap.org/data/2.5/forecast?lat=${lat}&lon=${lon}&units=imperial&appid=dce906a6d5620fa7b5e261e8bf5ff913`;
     fetch(apiUrl)
         .then(response => response.json())
         .then(data => {
@@ -75,9 +74,9 @@ const displayWeather = (weatherData) => {
 
 
 //days of weather 
-function weatherdays() {
-    var Days = moment().isoWeekday()
-}
+//function weatherdays() {
+  //  var Days = moment().isoWeekday()
+//}
 
 
 
