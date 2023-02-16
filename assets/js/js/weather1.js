50d03b754383da9309f6c4d139b3d0b061c11f55
AdibaSjd <adibasjd.id@gmail.com>
2023-02-16 21:36:49 +0000

    css

--- assets/js/js/weather1.js
+++ assets/js/js/weather1.js
@@ -5,6 +5,8 @@ var searchButton = document.querySelector('#search');
 
 searchButton.addEventListener('click', getlocation);
 
+var recentLocations = [];
+
 function getlocation(e) {
     e.preventDefault();
 
@@ -35,8 +37,18 @@ const searchlocation = function (search) {
             const locationData = data[0]
             const lat = locationData.lat;
             const lon = locationData.lon;
+
+            // Save the location to local storage so we can re-run the search later
+            saveLocation(search);
+
+            // Display the weather for the location
             getWeather(lat, lon);
         })
+}
+
+function onClickRecentLocation (e) {
+    var location = e.target.textContent;
+    searchlocation(location);
 }
 
 function getWeather (lat, lon) {
@@ -52,47 +64,72 @@ function getWeather (lat, lon) {
 
 //current weather displayed 
 const displayCurrentWeather = function (data) {
+    console.log(data);
     const currentWeather = data.current;
- 
-    document.querySelector('#temp_value').textContent = `${currentWeather.temp}`;
-    document.querySelector('#humid_value').textContent = `${currentWeather.humidity}%`;
-    document.querySelector('#wind_value').textContent = `${currentWeather.wind_speed}MPH`;
+    document.querySelector('#temp_value').textContent = `Temp: ${currentWeather.temp}°`;
+    document.querySelector('#humid_value').textContent = `Humidity: ${currentWeather.humidity}%`;
+    document.querySelector('#wind_value').textContent = `Wind Speed: ${currentWeather.wind_speed}MPH`;
 
     // Show the fortcast for the next 5 days
     displayWeatherForecast(data.daily);
 };
 
+
 const displayWeatherForecast = (dailyWeather) =>{
 
-    for (let i = 0; i < 5; i++) {
+    for (let i = 1; i < 6; i++) {
         const dayWeather = dailyWeather[i];
-        displayDayWeather(dayWeather);
+        displayDayWeather(dayWeather, i);
     };
 }
 
-const displayDayWeather = (weatherData) => {
-    console.log(weatherData);
-
-  var titleElem = document.createElement('h3');
-    titleElem.textContent = weatherData.dt;
-    document.querySelector('#forecast').appendChild(titleElem);
+const displayDayWeather = (weatherData, number) => {
     
-    return (cardHtml)
+     var dayName = document.getElementById("dayName" + number);
+     dayName.textContent = moment().add(number, 'days').format('dddd');
+
+     var dayTemp = document.getElementById("dayTemp" + number);
+     dayTemp.textContent = weatherData.temp.day;
+
+     var dayWind = document.getElementById("dayWind" + number);
+     dayWind.textContent = weatherData.wind_speed;
+
+     var dayHumidity = document.getElementById("dayHumidity" + number);
+     dayHumidity.textContent = weatherData.humidity;
 }
 
-//display weather on screen 
+// display weather on screen 
 const displayWeather = (weatherData) => {
     document.queryselector("#yourLocation").textContent = `${weatherData.name}, ${weatherData.country}`;
-
     getWeather(weatherData.lat, weatherData.lon);
 }
 
+// Save the location to local storage
+function saveLocation(location){
+    
+    recentLocations.push(location);
+    localStorage.setItem("location", JSON.stringify(recentLocations));
 
-//days of weather 
-//function weatherdays() {
-  //  var Days = moment().isoWeekday()
-//}
+    loadLocations();
+}
 
+// Load in the saved locations when the page first loads
+function loadLocations() {
+    var savedLocations = localStorage.getItem("location");
+    if(savedLocations){
+        recentLocations = JSON.parse(savedLocations);
 
+        var recentSearchesList = document.querySelector("#recentSearchesList");
+        recentSearchesList.innerHTML =""
+        for(var i = 0; i < recentLocations.length; i++){
+            var newLocation = document.createElement("li");
+            newLocation.textContent = recentLocations[i];
+            newLocation.onclick = onClickRecentLocation;
 
+            recentSearchesList.appendChild(newLocation);
+        }
+    }
+}
 
+// Load in the saved locations when the page first loads
+loadLocations();
